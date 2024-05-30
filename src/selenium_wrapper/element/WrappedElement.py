6b8f2e344ac8a6c84bd92fc12c4549ab211dce2d
Fedor Nesterovich <fnesterovich@mfsadmin.com>
2024-05-30 13:55:41 +0300

    v 1.0.6, refactor waits

--- src/selenium_wrapper/element/WrappedElement.py
+++ src/selenium_wrapper/element/WrappedElement.py
@@ -23,11 +23,13 @@ class WrappedElement:
     # Actions
 
     def click(self):
-        self.__get_web_driver_actions().click(self.wait_for_presence()).perform()
+        self.wait_for_presence()
+        self.__get_web_driver_actions().click(self.__get_web_element()).perform()
         logging.info(f"Clicking the Element located by {self.__by}: '{self.__locator}'")
 
     def double_click(self):
-        self.__get_web_driver_actions().double_click(self.wait_for_presence()).perform()
+        self.wait_for_presence()
+        self.__get_web_driver_actions().double_click(self.__get_web_element()).perform()
         logging.info(f"Double clicking the Element located by {self.__by}: '{self.__locator}'")
 
     def click_js(self):
@@ -42,29 +44,36 @@ class WrappedElement:
         logging.info(f"Clicking the invisible Element located by {self.__by}: '{self.__locator}'")
 
     def mouse_over(self):
-        self.__get_web_driver_actions().move_to_element(self.wait_for_presence()).perform()
+        self.wait_for_presence()
+        self.__get_web_driver_actions().move_to_element(self.__get_web_element()).perform()
         logging.info(f"Hovering over the Element located by {self.__by}: '{self.__locator}'")
 
     def mouse_down(self):
-        self.__get_web_driver_actions().click_and_hold(self.wait_for_presence())
+        self.wait_for_presence()
+        self.__get_web_driver_actions().click_and_hold(self.__get_web_element())
         logging.info(f"Clicking and holding LMB on the Element located by {self.__by}: '{self.__locator}'")
 
     def mouse_up(self):
-        self.__get_web_driver_actions().release(self.wait_for_presence())
+        self.wait_for_presence()
+        self.__get_web_driver_actions().release(self.__get_web_element())
         logging.info(f"Releasing LMB from the Element located by {self.__by}: '{self.__locator}'")
 
     def right_click(self):
-        self.__get_web_driver_actions().context_click(self.wait_for_presence()).perform()
+        self.wait_for_presence()
+        self.__get_web_driver_actions().context_click(self.__get_web_element()).perform()
         logging.info(f"Right clicking the Element located by {self.__by}: '{self.__locator}'")
 
     def drag_and_drop_to_element(self, element: "WrappedElement"):
-        self.__get_web_driver_actions().drag_and_drop(self.wait_for_presence(), element.wait_for_presence()).perform()
+        self.wait_for_presence()
+        element.wait_for_presence()
+        self.__get_web_driver_actions().drag_and_drop(self.__get_web_element(), element.__get_web_element()).perform()
         logging.info(
             f"Dragging the Element located by {self.__by}: '{self.__locator}' onto the Element located by "
             f"{element.__by}: '{element.__locator}'")
 
     def drag_and_drop_by_offset(self, xoffset: int, yoffset: int):
-        self.__get_web_driver_actions().drag_and_drop_by_offset(self.wait_for_presence(), xoffset, yoffset).perform()
+        self.wait_for_presence()
+        self.__get_web_driver_actions().drag_and_drop_by_offset(self.__get_web_element(), xoffset, yoffset).perform()
         logging.info(
             f"Dragging the Element located by {self.__by}: '{self.__locator}' "
             f"by X offset: {xoffset}, Y offset: {yoffset}")
@@ -92,7 +101,7 @@ class WrappedElement:
 
     def move_to_element_with_offset(self, element: "WrappedElement", xoffset: int, yoffset: int):
         self.wait_for_presence()
-        (self.__get_web_driver_actions().move_to_element_with_offset(element.wait_for_presence(), xoffset, yoffset)
+        (self.__get_web_driver_actions().move_to_element_with_offset(element.__get_web_element(), xoffset, yoffset)
          .perform())
         logging.info(f"Moving the mouse to the Element located by {self.__by}: '{self.__locator}' with "
                      f"X offset: {xoffset}, Y offset: {yoffset}")
@@ -104,7 +113,7 @@ class WrappedElement:
 
     def scroll_to_element(self, element: "WrappedElement"):
         self.wait_for_presence()
-        self.__get_web_driver_actions().scroll_to_element(element.wait_for_presence()).perform()
+        self.__get_web_driver_actions().scroll_to_element(element.__get_web_element()).perform()
         logging.info(f"Scrolling the Page to the Element located by {self.__by}: '{self.__locator}'")
 
     def clear_text(self):
@@ -132,43 +141,50 @@ class WrappedElement:
 
     # Get data from element
     def get_attribute(self, attribute: str):
-        element_attribute = self.wait_for_presence().get_attribute(attribute)
+        self.wait_for_presence()
+        element_attribute = self.__get_web_element().get_attribute(attribute)
         logging.info(f"Getting attribute: '{attribute}' from the Element located by {self.__by}: '{self.__locator}'. "
                      f"Returning '{element_attribute}'")
         return element_attribute
 
     def get_text(self):
-        element_text = self.wait_for_presence().text
+        self.wait_for_presence()
+        element_text = self.__get_web_element().text
         logging.info(f"Getting text from the Element located by {self.__by}: '{self.__locator}'. "
                      f"Returning '{element_text}'")
         return element_text
 
     def get_css_property(self, css_property: str):
-        element_css_property = self.wait_for_presence().value_of_css_property(css_property)
+        self.wait_for_presence()
+        element_css_property = self.__get_web_element().value_of_css_property(css_property)
         logging.info(f"Getting CSS property: '{css_property}' from the Element located by "
                      f"{self.__by}: '{self.__locator}'. Returning '{element_css_property}'")
         return element_css_property
 
     def get_class_name(self):
-        element_class = self.wait_for_presence().get_attribute("class")
+        self.wait_for_presence()
+        element_class = self.__get_web_element().get_attribute("class")
         logging.info(f"Getting class name from the Element located by "
                      f"{self.__by}: '{self.__locator}'. Returning '{element_class}'")
         return element_class
 
     def get_id(self):
-        element_id = self.wait_for_presence().get_attribute("id")
+        self.wait_for_presence()
+        element_id = self.__get_web_element().get_attribute("id")
         logging.info(f"Getting id from the Element located by "
                      f"{self.__by}: '{self.__locator}'. Returning '{element_id}'")
         return element_id
 
     def get_value(self):
-        element_value = self.wait_for_presence().get_attribute("value")
+        self.wait_for_presence()
+        element_value = self.__get_web_element().get_attribute("value")
         logging.info(f"Getting value from the Element located by "
                      f"{self.__by}: '{self.__locator}'. Returning '{element_value}'")
         return element_value
 
     def get_size(self) -> dict:
-        element_size = self.wait_for_presence().size
+        self.wait_for_presence()
+        element_size = self.__get_web_element().size
         logging.info(f"Getting size from the Element located by {self.__by} '{self.__locator}'. "
                      f"Returning {element_size}")
         return element_size
@@ -198,13 +214,15 @@ class WrappedElement:
             return False
 
     def is_visible(self) -> bool:
-        is_element_visible = self.wait_for_presence().is_displayed()
+        self.wait_for_presence()
+        is_element_visible = self.__get_web_element().is_displayed()
         logging.info(f"Checking visibility of the Element located by {self.__by} '{self.__locator}'. "
                      f"Returning {is_element_visible}")
         return is_element_visible
 
     def is_clickable(self) -> bool:
-        is_element_clickable = self.wait_for_presence().is_enabled()
+        self.wait_for_presence()
+        is_element_clickable = self.__get_web_element().is_enabled()
         logging.info(f"Checking if the Element located by {self.__by} '{self.__locator}' is clickable. "
                      f"Returning {is_element_clickable}")
         return is_element_clickable
@@ -233,92 +251,105 @@ class WrappedElement:
         logging.debug(
             f"Waiting for the Element located by {self.__by}: '{self.__locator}' to be present. "
             f"Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.presence_of_element_located(self.__get_element_by_and_locator()))
+        return self
 
     def wait_for_attribute_in_element(self, attribute: str, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for the attribute '{attribute}' to be included in the Element located by {self.__by}: "
             f"'{self.__locator}'. Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.element_attribute_to_include(self.__get_element_by_and_locator(), attribute))
+        return self
 
     def wait_until_selected(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for the Element located by {self.__by}: '{self.__locator}' to be selected. "
             f"Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.element_located_to_be_selected(self.__get_element_by_and_locator()))
+        return self
 
     def wait_until_clickable(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for the Element located by {self.__by}: '{self.__locator}' to be clickable. "
             f"Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.element_to_be_clickable(self.__get_element_by_and_locator()))
+        return self
 
     def wait_for_visibility(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for the Element located by {self.__by}: '{self.__locator}' to be visible. "
             f"Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.visibility_of_element_located(self.__get_element_by_and_locator()))
+        return self
 
     def wait_for_invisibility(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for the Element located by {self.__by}: '{self.__locator}' to be invisible. "
             f"Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.invisibility_of_element_located(self.__get_element_by_and_locator()))
+        return self
 
     def wait_for_absence(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for the Element located by {self.__by}: '{self.__locator}' to be absent. "
             f"Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.staleness_of(self.__get_web_element()))
+        return self
 
     def wait_until_text_is_present_in_element(self, text: str, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for text '{text}' to be present in the Element located by {self.__by}: '{self.__locator}'. "
             f"Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.text_to_be_present_in_element(self.__get_element_by_and_locator(), text))
+        return self
 
     def wait_until_text_is_present_in_attribute(self, attribute: str, text: str, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for text '{text}' to be present in the attribute '{attribute}' of the Element located by "
             f"{self.__by}: '{self.__locator}'. Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.text_to_be_present_in_element_attribute(self.__get_element_by_and_locator(), attribute, text))
+        return self
 
     def wait_until_text_is_present_in_value(self, text: str, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for text '{text}' to be present in the value of the Element located by {self.__by}: "
             f"'{self.__locator}'. Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.text_to_be_present_in_element_value(self.__get_element_by_and_locator(), text))
+        return self
 
     def wait_for_visibility_of_all_elements(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting all Elements located by {self.__by}: '{self.__locator}' to be visible. Timeout set to {timeout} "
             f"seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.visibility_of_all_elements_located(self.__get_element_by_and_locator()))
+        return self
 
     def wait_for_presence_of_all_elements(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for all Elements located by {self.__by}: '{self.__locator}' to be present. "
             f"Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.presence_of_all_elements_located(self.__get_element_by_and_locator()))
+        return self
 
     def wait_for_visibility_of_any_of_the_elements(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
         logging.info(
             f"Waiting for all Elements located by {self.__by}: '{self.__locator}' to be visible. "
             f"Timeout set to {timeout} seconds")
-        return self.__get_web_driver_wait(timeout).until(
+        self.__get_web_driver_wait(timeout).until(
             ec.visibility_of_any_elements_located(self.__get_element_by_and_locator()))
+        return self
 
     # WebDriver related
     def __get_web_element(self) -> WebElement:
@@ -340,9 +371,7 @@ class WrappedElement:
         return self.__by, self.__locator
 
     def wait_for_visibility_and_click(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
-        self.wait_for_visibility(timeout)
-        self.click()
+        self.wait_for_visibility(timeout).click()
 
     def wait_until_clickable_and_click(self, timeout=__DEFAULT_TIME_OUT_SECONDS):
-        self.wait_until_clickable(timeout)
-        self.click()
+        self.wait_until_clickable(timeout).click()
