2f2e73d811ae968136528ce916064016fcee41e0
Fedor Nesterovich <fnesterovich@mfsadmin.com>
2024-05-30 12:46:11 +0300

    v 1.0.4, add requirements.txt and webdriver-manager to dependencies

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
    name='selenium_wrapper',
-   version='1.0.3',
+   version='1.0.4',
    description='Wrapper for Selenium Webdriver',
    author='Fedor Nesterovich',
    author_email='fnesterovich@mfsadmin.com',
    packages=find_packages(),
-   install_requires=['selenium']
+   install_requires=['selenium', 'webdriver-manager']
 )
