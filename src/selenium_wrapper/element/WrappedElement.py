0b4e0e21441b80a2baf55dc83b97db2b2b737405
Fedor Nesterovich <fnesterovich@mfsadmin.com>
2024-06-13 10:01:33 +0300

    v 1.0.47

--- src/selenium_wrapper/element/WrappedElement.py
+++ src/selenium_wrapper/element/WrappedElement.py
@@ -369,11 +369,10 @@ class WrappedElement:
         try:
             if self.__web_element is None:
                 logging.info(f"Locating Element by {self.__by}: {self.__locator}")
-                webelement = WebDriverSingleton.get_driver().find_element(self.__by, self.__locator)
-                return webelement
+                return WebDriverSingleton.get_driver().find_element(self.__by, self.__locator)
             else:
                 return self.__web_element
-        except NoSuchElementException or JavascriptException as e:
+        except (NoSuchElementException, JavascriptException) as e:
             raise NoSuchElementException(f"Unable to locate an element with the {self.__by}: {self.__locator}"
                                          f" in the current page!") from e
 
